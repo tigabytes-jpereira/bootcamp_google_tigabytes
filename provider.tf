05cd70fb94e7475ce5d38dfa9b3fa43399b53a04
tigabytes-jpereira <jean.pereira@tigabytes.com>
2025-05-21 15:15:42 -0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.0" # Use a versão mais recente compatível
+      version = ">= 5.0" # Use a versão mais recente compatível
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 6.0" # Use a versão mais recente compatível
+      version = ">= 6.0" # Use a versão mais recente compatível
     }
   }
 }
